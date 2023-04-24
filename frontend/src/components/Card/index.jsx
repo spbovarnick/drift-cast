92a3acd172c050dae1d5b95c4f576c05179a53a2
spbovarnick <spbovarnick@gmail.com>
2023-04-24 09:30:55 -0700

    added tailwind config safelist to render template literal class names, which should be way more visible in tailwind documentation

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -8,36 +8,48 @@ export default function Card({ riverData }) {
 
 
     if (riverData.height <= riverData.goodLow ) {
-        console.log("too low")
+        // console.log("too low")
         conditions = {description: "Too Low", color: "blue-950"}
     } else if (riverData.goodLow < riverData.height && riverData.height < riverData.goodHigh ) {
-        console.log("good")
+        // console.log("good")
         conditions = {description: "Good", color: "teal-800"}
     } else if (riverData.goodHigh < riverData.height && riverData.height <= riverData.perfectHigh  ) {
-        console.log("perfect")
+        // console.log("perfect")
         conditions = {description: "Perfect", color: "green-600"}
     } else if (riverData.perfectHigh < riverData.height && riverData.height <= riverData.highHigh ) {
-        console.log("high")
+        // console.log("high")
         conditions = {description: "High", color: "yellow-400"}
     } else if (riverData.highHigh < riverData.height && riverData.height <= riverData.tooHighHigh ) {
-        console.log("too high")
+        // console.log("too high")
         conditions = {description: "Too high", color: "orange-500"}
     } else if (riverData.tooHighHigh < riverData.height) {
-        console.log("serious danger")
+        // console.log("serious danger")
         conditions = {description: "Serious Danger", color: "red-600"}
     }
     
 
-    return (
-        <div className={`border-2 border-${conditions.color}`}>
-            <p className="">{riverData.name}</p>
-            <iframe
-                className=""
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-                src={`https://www.google.com/maps/embed/v1/place?key=${import.meta.env.VITE_GOOGLE_API_KEY}&q=${riverData.latitude},${riverData.longitude}&maptype=satellite&zoom=7`}
-             />
+    let renderCard = <p>River data is loading...</p>
+    if (riverData) {
+        renderCard = <div className={`border-2 border-${conditions.color}`}>
+            <p className={`text-${conditions.color}`}>{riverData.name}</p>
+            <div>
+                <iframe
+                    className=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    src={`https://www.google.com/maps/embed/v1/place?key=${import.meta.env.VITE_GOOGLE_API_KEY}&q=${riverData.latitude},${riverData.longitude}&maptype=satellite&zoom=7`}
+                />
+                <div className={`h-52 w-52 border-2 border-${conditions.color} rounded-full bg-${conditions.color}`}>
+                </div>
+            </div>
 
         </div>
+    }
+
+
+    return (
+        <>
+            {renderCard}
+        </>
     )
 }
