fdb0736c11efbc9d864c3566d1d11178c84da8fc
spbovarnick <spbovarnick@gmail.com>
2023-04-26 16:59:55 -0700

    added about link to hero

--- frontend/src/components/HomePage/index.jsx
+++ frontend/src/components/HomePage/index.jsx
@@ -1,4 +1,4 @@
-import hero from "../../assets/static/hero.jpg"
+import { Link } from "react-router-dom"
 import "./styles.css"
 
 export default function HomePage({ allRivers }) {
@@ -11,7 +11,9 @@ export default function HomePage({ allRivers }) {
                 <div id="hero" className="drop-shadow-lg text-blue-800 flex flex-col justify-center h-full">
                     <p className={`font-serif text-xl ${backdrop}`}>DriftCast</p>
                     <p className={`${backdrop} text-sm md:text-base`}>River and stream condition tracker</p>
-                    <button className={`${backdrop} self-center mt-20 rounded-full rounded-r-full hover:opacity-70 active:shadow-inner active:outline active:outline-2 outline-blue-400`}>Learn More</button>
+                    <Link to="/about">
+                        <p className={`${backdrop} self-center mt-20 rounded-full rounded-r-full hover:opacity-70 active:shadow-inner active:outline active:outline-2 outline-blue-400`}>Learn More</p>
+                    </Link>
                 </div>
             </div>
             <div className='flex justify-items-center grid grid-cols-1 lg:grid-cols-2 m-16 gap-4'>
