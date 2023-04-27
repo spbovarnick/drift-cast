c17f226de561f4aa932a33575ba8c495715105d6
spbovarnick <spbovarnick@gmail.com>
2023-04-27 14:02:15 -0700

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,9 @@ const path = require('path')
 const express = require('express')
 const cors = require('cors')
 
-
-
 /* Required routes in controllers folder
 -------------------------------------------- */
 const reportCtrl = require('./controllers/reports')
-
 
 /* Required db connection, models
 -------------------------------------------- */
@@ -20,21 +17,30 @@ const db = require('./models')
 -------------------------------------------- */
 const app = express();
 
-
 /* Middleware
 -------------------------------------------- */
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 /* look at controllers to handle routes
 -------------------------------------------- */
 app.use('/api/reports', reportCtrl);
-
 
 /* listen to specified port
 -------------------------------------------- */
 app.listen(process.env.PORT, function () {
     console.log('Express is listening to port', process.env.PORT);
+});
+
+/* use React build folder for static files
+-------------------------------------------- */
+app.use(express.static(path.join(path.dirname(__dirname), 'frontend', 'dist')))
+
+
+/* any other route not matching above routes gets
+routed by React
+-------------------------------------------- */
+app.get('*', (req, res) => {
+    res.sendFile(path.join(path.dirname(__dirname), 'frontend', 'dist', 'index.html'));
 });
