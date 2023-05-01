c6360560c642d4c99c65c09b7709829e1ac5e24f
spbovarnick <spbovarnick@gmail.com>
2023-04-30 17:30:49 -0700

    typo fixes

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -8,12 +8,12 @@ export default function About() {
                 <p className="text-center text-xl font-bold">About</p><br/><br/>
                 <div className="flex flex-col md:flex-row md:self-center nd">
                     <div className=" m-2" >
-                        <p>Taking notes from surf forecasting apps like Magic Seaweed and Surfline, DriftCast is the product of its creators singular obsession with fly fishing. Previously, forecasting fishing conditions required accessing the barebones National Weather Service hydrography pages and cross-referencing those real-time datum with experiential notes. <br/><br/>
+                        <p>Taking notes from surf forecasting apps like Magic Seaweed and Surfline, DriftCast is the product of its creator's singular obsession with fly fishing. Previously, forecasting fishing conditions required accessing the barebones National Weather Service hydrography pages and cross-referencing those real-time datum with experiential notes. <br/><br/>
                         
                         But what's an angler to do if they haven't visited a stream before? 
-                        Typically tight-lipped, getting information about a river or stream from other anglers is a notoriously difficult task. <br/><br/>
+                        Typically tight-lipped, many anglers aren't keen to share information about a rivers or streams. <br/><br/>
                         
-                        DriftCast throws the gates wide oepn. We use the USGS API to get real-time gage heighths and flow volumes. Those data points are then checked against condition ranges developed from years of experience in the water. No matter your level of experience on a given stream, DriftCast provides a one-stop shop for checking conditions. <br/><br/>
+                        DriftCast throws the gates wide oepn. We use the USGS and NWS APIs to get real-time gage heighths, flow volumes, and atmospheric conditions. Those data points are then checked against condition ranges developed from years of experience in the water. No matter your level of experience on a given stream, DriftCast provides a one-stop shop for checking conditions. <br/><br/>
                         </p>
                     </div>
                     <div className="flex justify-center items-center">
